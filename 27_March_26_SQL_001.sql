9152cea29b66371f3515c9e19c4b021ecf5a4c10
Shubham Vishwakarma <shubhampc40k@gmail.com>
2026-04-02 17:00:56 +0530

    Delete 27_March_26_SQL_001.sql

--- 27_March_26_SQL_001.sql
+++
@@ -1,7 +0,0 @@
--- SQL Challenge 1: Retrieve all employees with salary > 50000
--- Table: Employees(emp_id, emp_name, department, salary)
-
-SELECT emp_id, emp_name, department, salary
-FROM Employees
-WHERE salary > 50000
-ORDER BY salary DESC;
